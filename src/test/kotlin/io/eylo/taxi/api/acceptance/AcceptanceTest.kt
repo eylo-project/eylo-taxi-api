a2b18c7a550d8e7b48bfa93dfb72cccd160583de
Lyle Son <calends99@gmail.com>
2022-10-19 14:41:37 +0900

    [ISSUE-1] health check API 테스트 추가

---
+++ src/test/kotlin/io/eylo/taxi/api/acceptance/AcceptanceTest.kt
@@ -0,0 +1,22 @@
+package io.eylo.taxi.api.acceptance
+
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.restdocs.RestDocumentationExtension
+import org.springframework.test.context.TestConstructor
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@ExtendWith(RestDocumentationExtension::class, SpringExtension::class)
+internal abstract class AcceptanceTest {
+
+    @Autowired
+    protected lateinit var mockMvc: MockMvc
+}
