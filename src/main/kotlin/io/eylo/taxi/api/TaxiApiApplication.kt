1aec5e499349708521ff41acd18f6be42c95f4c0
Lyle Son <calends99@gmail.com>
2022-10-23 20:26:15 +0900

    [ISSUE-1] Application 이름 변경

--- src/main/kotlin/io/eylo/taxi/api/TaxiApiApplication.kt
+++ src/main/kotlin/io/eylo/taxi/api/TaxiApiApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class EyloTaxiApiApplication
+class TaxiApiApplication
 
 fun main(args: Array<String>) {
-    runApplication<EyloTaxiApiApplication>(*args)
+    runApplication<TaxiApiApplication>(*args)
 }
