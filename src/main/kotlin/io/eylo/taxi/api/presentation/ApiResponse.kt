e1cd8cbb53560369e8513f4ed1b3499d3e0a14e5
minju <bbak0402dd@gmail.com>
2022-11-01 15:58:58 +0900

    [#5] 세미콜론 삭제, OK 메서드 추가, ERROR 메서드 삭제

--- src/main/kotlin/io/eylo/taxi/api/presentation/ApiResponse.kt
+++ src/main/kotlin/io/eylo/taxi/api/presentation/ApiResponse.kt
@@ -1,20 +1,21 @@
 package io.eylo.taxi.api.presentation
 
 class ApiResponse<T> private constructor(
-        val data: T?,
+        val data: T,
         val message: String?
 ) {
     companion object {
-        fun <T> OK(data: T): ApiResponse<T> {
-            return ApiResponse(data, null);
+        fun OK(): ApiResponse<Any?> {
+            return ApiResponse(null, null)
         }
 
-        fun ERROR(throwable: Throwable): ApiResponse<*> {
-            return ApiResponse(null, throwable.message);
+        fun <T> OK(data: T): ApiResponse<T> {
+            return ApiResponse(data, null);
+
         }
 
         fun ERROR(message: String): ApiResponse<*> {
-            return ApiResponse(null, message);
+            return ApiResponse(null, message)
         }
     }
 }
