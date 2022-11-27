181c234daab8a266447a885b5c0dc44603c97175
Lyle Son <calends99@gmail.com>
2022-11-27 18:37:59 +0900

    [#8] 필요없는 어노테이션 제거

--- src/main/kotlin/io/eylo/taxi/api/domain/rider/UsageHistoryRepository.kt
+++ src/main/kotlin/io/eylo/taxi/api/domain/rider/UsageHistoryRepository.kt
@@ -1,9 +1,7 @@
 package io.eylo.taxi.api.domain.rider
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 
-@Repository
 interface UsageHistoryRepository : JpaRepository<UsageHistory, Long> {
 
     fun findAllByRiderId(riderId: Long): List<UsageHistory>
